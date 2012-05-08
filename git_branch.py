e71cc4fc0aa8934356a6b6b52a5f03f322e57d88
tcsavage <tcsavage@gmail.com>
2012-05-08 01:33:46 +0100

    Added GPLv3 license and removed commented line

--- git_branch.py
+++ git_branch.py
@@ -1,3 +1,21 @@
+# Git Branch plugin for Sublime Text 2
+# (Displays the name of the current branch in the status bar)
+# Copyright (C) 2012 Tom Savage
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
 import sublime, sublime_plugin, os.path
 
 class GitBranchListener(sublime_plugin.EventListener):
@@ -15,7 +33,6 @@ def setBranchMsg():
 	else:
 		filename = os.path.split(sublime.active_window().active_view().file_name())[1]
 		branch = getBranchName(gitpath)
-		#sublime.status_message("%s on branch %s" %(filename, branch))
 		sublime.active_window().active_view().set_status("gitbranch", "%s on branch %s" %(filename, branch))
 
 def searchUpForGit(path):
